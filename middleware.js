d8fa47c8a1fdf38c6834ae59bb6a95048f4af456
SkalekMarek <skalin.junior6@gmail.com>
2026-04-09 23:52:18 +0200

    fix: explicit nodejs runtime for middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,15 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
+// This tells Vercel to use the full Node.js environment instead of the "Edge"
+export const runtime = 'nodejs';
+
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
